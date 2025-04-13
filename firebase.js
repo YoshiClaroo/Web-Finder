3bc5268423fbd6e8b81001d5413672e979ef4bb9
YoshiClaroo <braianm313@gmail.com>
2025-04-13 13:44:08 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,40 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, updateDoc, increment, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  increment,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// Configuración de tu proyecto Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMAIN.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDrvltDfr_3ioGeU63I_XnO-915yx7LiB0",
+  authDomain: "web-finder-7dbd5.firebaseapp.com",
+  projectId: "web-finder-7dbd5",
+  storageBucket: "web-finder-7dbd5.firebasestorage.app",
+  messagingSenderId: "256985326029",
+  appId: "1:256985326029:web:b15eeaf841a399ae1fe484",
+  measurementId: "G-NVHD1F5RS6"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+
+// Obtener servicios de Firebase
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+// Exportar solo lo necesario
 export {
   auth,
   db,
